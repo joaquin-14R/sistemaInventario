214ed1c5cee0e0a8a92ca20d04e0ec1b16435315
Enola Carely Rodriguez Gonzalez <57816906+EnolaByte@users.noreply.github.com>
2021-10-17 13:57:22 -0700

    Señalacion
    Se agrego esa parte del titulo para saber donde nos encontramos en el sistema

--- producto_agotado.php
+++ producto_agotado.php
@@ -22,7 +22,7 @@ $usuario = $_SESSION['usuario'];
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="description" content="">
-	<title>Sistema de Inventario</title>
+	<title>Sistema de Inventario / Producto agotado</title>
 	<!-- CSS only -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<!-- JavaScript Bundle with Popper -->
